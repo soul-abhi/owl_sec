d6ab475cbb41d9068c6ee982602cdcfcf219ece5
abhishek-kiet <abhishek.2428cse1292@kiet.edu>
2026-02-02 12:02:10 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
 export default function HomePage() {
   return (
     <main>
-      <nav>
-        <ul style={{
-          background:'white',
-          color:'black',
-          listStyleType: 'none',
-          display: 'flex',
-          gap: '50px',
-          padding: '10px',
-          alignContent: 'center',
-          justifyContent: 'right',
-          paddingRight: '50px'
-        }}>
-          <li><a href="/about">About Us</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-
-      </nav>
+      
     </main>
   );
 }
+
 
